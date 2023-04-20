ba946a2aa573aedcda9fd5619db41b3264a5404a
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-20 09:34:34 +0500

    08

--- Slicing08.py
+++ Slicing08.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         str: answer
     """
-    return
+    x1=s[0]
+    x2=s[1]
+    x3=s[2]
+    x4=s[3]
+    x5=s[4]
+    return x2+x4+x1+x3+x5
+print(main("apple"))
